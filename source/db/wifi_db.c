1da32a17c4950b952d85059167bc3eb79f02b5d0
MG-kildon <MARIAGNANAKILDON_S@comcast.com>
2026-01-19 12:24:33 +0530

    bandwidth change

--- source/db/wifi_db.c
+++ source/db/wifi_db.c
@@ -110,7 +110,7 @@ static int init_radio_config_default(int radio_index, wifi_radio_operationParam_
         case WIFI_FREQUENCY_5L_BAND:
             cfg.operatingClass = 128;
             cfg.channel = 36;
-            cfg.channelWidth = WIFI_CHANNELBANDWIDTH_80MHZ;
+            cfg.channelWidth = WIFI_CHANNELBANDWIDTH_160MHZ;
 #if defined (_HUB4_PRODUCT_REQ_) && !defined (_SR213_PRODUCT_REQ_)
             cfg.variant = WIFI_80211_VARIANT_A | WIFI_80211_VARIANT_N | WIFI_80211_VARIANT_AC;
 #else
@@ -124,7 +124,7 @@ static int init_radio_config_default(int radio_index, wifi_radio_operationParam_
         case WIFI_FREQUENCY_5H_BAND:
             cfg.operatingClass = 128;
             cfg.channel = 157;
-            cfg.channelWidth = WIFI_CHANNELBANDWIDTH_80MHZ;
+            cfg.channelWidth = WIFI_CHANNELBANDWIDTH_160MHZ;
             cfg.variant = WIFI_80211_VARIANT_A | WIFI_80211_VARIANT_N | WIFI_80211_VARIANT_AC | WIFI_80211_VARIANT_AX;
 
 #ifdef CONFIG_IEEE80211BE
